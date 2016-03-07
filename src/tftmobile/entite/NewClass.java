9d54c3974685753d7c11d899e7d68c3dd8f49e44
malox94 <malox94@192.168.1.3>
2016-03-07 21:31:45 +0100

    Test Pour le Commit

---
+++ src/tftmobile/entite/NewClass.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tftmobile.entite;
+
+/**
+ *
+ * @author malox94
+ */
+public class NewClass {
+    
+}
